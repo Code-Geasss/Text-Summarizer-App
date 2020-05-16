0adbe467b2e29cbfe19caa071b5ea949cf94e683
Chintan Modi <chintan.modi@somaiya.edu>
2020-05-16 21:01:49 +0530

    Revert "Second"
    This reverts commit f698639d721403d59e039ab21021081be3fd3da1.

--- app/src/main/java/com/example/loginapp/Member.java
+++ app/src/main/java/com/example/loginapp/Member.java
@@ -12,13 +12,12 @@ public class Member {
     public void setInput(String input) {
         this.input = input;
     }
-
     public String getOutput() {
-        return output;
+        return input;
     }
 
-    public void setOutput(String output) {
-        this.output = output;
+    public void setOutput(String input) {
+        this.input = input;
     }
 
 }
