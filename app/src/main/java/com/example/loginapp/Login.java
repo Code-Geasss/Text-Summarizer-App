3628820e47e7539ab3160ea55b84aa2ea099e8b7
vmundra <krishnamundra91@gmail.com>
2020-05-20 00:31:55 +0530

    final commit

---
+++ app/src/main/java/com/example/loginapp/Login.java
@@ -0,0 +1,103 @@
+package com.example.loginapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    EditText mEmail,mPassword;
+    FloatingActionButton mLoginBtn;
+    TextView reglink,forgotTextLink;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmail=findViewById(R.id.Email);
+        mPassword=findViewById(R.id.password);
+        fAuth=FirebaseAuth.getInstance();
+        reglink = (TextView)findViewById(R.id.reglink);
+        mLoginBtn=findViewById(R.id.loginBtn);
+        forgotTextLink=findViewById(R.id.forgotPassword);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=mEmail.getText().toString().trim();
+                String password=mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required");
+                    return;
+                }
+
+                if (password.length()<6){
+                    mPassword.setError("Password must be >= 6 characters");
+                    return;
+                }
+
+                //progressBar.setVisibility(View.VISIBLE);
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else {
+                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            //progressBar.setVisibility(View.GONE);
+                        }
+
+                    }
+                });
+            }
+        });
+
+        reglink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regIntent = new Intent(Login.this,Register.class);
+                startActivity(regIntent);
+                overridePendingTransition(R.anim.slide_from_right,R.anim.slideout_from_left);
+            }
+        });
+
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,ForgotPasswordActivity.class));
+                overridePendingTransition(R.anim.slide_from_right,R.anim.slideout_from_left);
+            }
+        });
+
+    }
+}
